9ddce119a9ab9090206f95f6969cbb7f608cea16
= <=>
2020-10-02 11:08:14 +0300

    removed .env to store my secret

--- Futatips/settings.py
+++ Futatips/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =os.getenv("Mysecret") #'9ajr%8ve0+y9hdh48_g$vm+ty87&1+hy&#uxdp&qg16%!o@6$o'
+SECRET_KEY ='9ajr%8ve0+y9hdh48_g$vm+ty87&1+hy&#uxdp&qg16%!o@6$o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
